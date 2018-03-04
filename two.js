a19faaa62ec7a1b710b351e85bc84f877b4cdfe0
gayathridevi007 <gayathriprathi27@gmail.com>
2018-03-04 12:05:57 +0530

    modified files adding for first

--- two.js
+++ two.js
@@ -14,7 +14,9 @@ getfile("one.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
   basicinfo(data.basics);
+  education(data.education);
 });
+var main=document.querySelector(".flex-parent");
 function basicinfo(basic){
   var profile=document.getElementById("basics");
   var name=document.createElement("h2");
@@ -31,3 +33,37 @@ function basicinfo(basic){
   profile.appendChild(photo);
 
 }
+var right=document.createElement("div");
+right.classList.add("content-child");
+main.appendChild(right);
+function education(education){
+var e1=document.createElement("div");
+e1.classList.add("edu");
+right.appendChild(e1);
+var h1=document.createElement("h1");
+h1.setAttribute("id","heading");
+h1.textContent="Education Details";
+h1.appendChild(document.createElement("HR"));
+//console.log(right);
+ e1.appendChild(h1);
+  //function educationinfo(education){
+   for(i in education){
+           var h2=document.createElement("h2");
+             h2.classList.add("edu1");
+    h2.innerHTML=education[i].course;
+          h1.appendChild(h2);
+      var h3=document.createElement("h3");
+      h3.classList.add("edu2");
+      h3.textContent=education[i].college;
+      h2.appendChild(h3);
+      var ul=document.createElement("ul");
+      ul.classList.add("edu3");
+      h3.appendChild(ul);
+      for(j in education){
+      var li=document.createElement("li")
+      li.textContent=education[i].data[j];
+      ul.appendChild(li);
+    }
+
+    }
+ }
